fcf961078bc0062025fdd1288d2c9a8a1f25dd41
Roma <roma.topchiy@me.com>
2020-05-19 13:24:21 +0300

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Roman Topchii on 19.05.2020.
+//  Copyright © 2020 Roman Topchii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
